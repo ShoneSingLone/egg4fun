805a23a1643f44a0991919963b56b178dfc79d1b
ShoneSingLone <ShoneSingLone@gmail.com>
2019-05-07 08:48:04 +0800

    update user

--- database/migration/20190506151358-create-user.js
+++ database/migration/20190506151358-create-user.js
@@ -1,27 +1,39 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', {
+  up: (queryInterface, {
+    INTEGER,
+    STRING,
+    DATE,
+    TINYINT
+  }) => {
+    return queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: INTEGER,
       },
-      componenttype: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
+      email: STRING(40),
+      password: STRING,
+      inviter_id: INTEGER,
+      username: STRING(40),
+      weibo: STRING(40),
+      weixin: STRING(40),
+      team_id: INTEGER,
+      receive_remote: TINYINT(1),
+      email_verifyed: TINYINT(1),
+      avatar: STRING(40),
+      created_at: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: DATE,
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('users');
-  }
+    return queryInterface.dropTable('Users');
+  },
 };
