aa6e0da2139d3429179fd440f80f805ea3577331
ShoneSingLone <ShoneSingLone@gmail.com>
2020-05-14 23:55:24 +0800

    update restful

--- config/config.default.js
+++ config/config.default.js
@@ -35,6 +35,12 @@ module.exports = appInfo => {
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
+    // 加载 errorHandler 中间件
+    middleware: [ 'errorHandler' ],
+    // 只对 /api 前缀的 url 路径生效
+    errorHandler: {
+      match: '/api',
+    },
   };
 
   config.sequelize = {
@@ -43,6 +49,25 @@ module.exports = appInfo => {
     port: 3306,
     database: 'egg4fun',
     password: env('MYSQL_PWD'),
+  };
+  config.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: '127.0.0.1',
+      // 端口号
+      port: 3306,
+      // 用户名
+      user: env('MYSQL_USER'),
+      // 密码
+      password: env('MYSQL_PWD'),
+      // 数据库名
+      database: 'egg4fun',
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
   };
 
   return {
--- database/seeders/20200428161155-creat_user.js
+++ database/seeders/20200428161155-creat_user.js
@@ -1,4 +1,5 @@
 'use strict';
+const utils = require('utility');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -8,10 +9,21 @@ module.exports = {
 
       Example:
       */
-    return queryInterface.bulkInsert('Users', [{
-      name: 'John Doe',
-      isBetaMember: false,
-    }], {});
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'SingLone@foxmail.com',
+        password: utils.md5('000000'),
+        inviter_id: 0,
+        username: 'SSL',
+        weixin: 'xxxx',
+        weibo: 'xxxx',
+        receive_remote: 0,
+        email_verifyed: 1,
+        avatar: 'xxxx.jpg',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
